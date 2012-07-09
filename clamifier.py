30093224b56b80d6a49c6fed0acefebb6e7f528a
GiM <gim913@gmail.com>
2012-07-09 18:03:22 +0200

    data parser beginning

--- clamifier.py
+++ clamifier.py
@@ -3,6 +3,12 @@
 # vim: tabstop=4 shiftwidth=4 noexpandtab 
 #
 
+class DataException(Exception):
+	def __init__(self, value):
+		self.value = value
+	def __str__(self):
+		return repr(self.value)
+	
 class TypeDesc:
 	def __init__(self, desc):
 		self.isAnd = False
@@ -29,18 +35,52 @@ class TypeDesc:
 					self.values[int(val)] = 1
 		print self.values, desc
 
+class DataParser:
+	def __init__(self, dataLines):
+		self.data = dataLines
+		for line in dataLines:
+			line = line.strip()
+			if not len(line):
+				continue
+
+			if line[0] == '"':
+				parseString(line)
+			elif line[0] == "'":
+				parseAscii(line)
+			else:
+				parseNumbers(line)
+		raise DataException('dummy')
+
+	def parseString(self, line):
+		pass
+
+	def parseAscii(self, line):
+		pass
+
+	def parseNumbers(self, line):
+		pass
+
 class SigParser:
-	def __init__(self, sig):
+	def __init__(self, sig, lineNumber):
 		self.sig = sig
+		self.sigLineNumber = lineNumber
 
 	def parse(self):
-		for s in self.sig:
+		data = None
+		self.title = '[empty]'
+		for idx,s in enumerate(self.sig):
 			if s[0:6] == "TITLE:":
 				self.title = s[6:].replace(':', ';')
 				continue
 			if s[0:5] == "TYPE:":
 				self.typeDesc = TypeDesc(s[5:])
 				continue
+			if s[0:5] == "DATA:":
+				data = self.sig[idx+1:]
+				continue
+		if not data:
+			raise DataException('error in data of signature at line ' + str(self.sigLineNumber) + ' named: ' + self.title)
+		self.data = DataParser(data)
 
 class DbParser:
 	def __init__(self, filename):
@@ -49,18 +89,22 @@ class DbParser:
 	def parse(self):
 		sig = []
 		counter = 0
+		lineNo  = 0
+		sigLine = 1
 		while True:
 			line = self.fd.readline()
 			if not line:
 				break
 			line = line.strip()
+			lineNo += 1
 			if line != "----":
 				sig.append(line)
 			else:
-				sp = SigParser(sig)
+				sp = SigParser(sig, sigLine)
 				sp.parse()
 				sig = []
 				counter += 1
+				sigLine = lineNo+1
 #				print "\r",counter,
 		return 0
 
